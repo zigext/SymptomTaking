6f239d27c5e1d3a840bfe49b1fba9e1eb48c23c6
zigext <zigext@gmail.com>
2018-06-13 14:02:35 +0700

    Use ThemeProvider from material-ui

---
+++ App.js
@@ -0,0 +1,43 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  Platform,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { BottomNavigation, COLOR, ThemeProvider, Toolbar, Card, Checkbox, RadioButton, Icon, IconToggle } from 'react-native-material-ui';
+import VectorIcon from 'react-native-vector-icons/MaterialIcons'
+import SymptomTakingScreen from './screens/SymptomTakingScreen'
+
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <ThemeProvider uiTheme={uiTheme}>
+
+        <SymptomTakingScreen />
+
+      </ThemeProvider>
+    );
+  }
+}
+
+  const uiTheme = {
+    palette: {
+      primaryColor: "#7ec8ba",
+      
+    },
+    toolbar: {
+      container: {
+        height: 45
+      }
+    }
+
+  }
