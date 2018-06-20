6e01181ddf5e8c88881e391eb8e1c7aa2d616135
zigext <zigext@gmail.com>
2018-06-20 20:36:43 +0700

    Answer with P type (upload picture)

---
+++ components/AnswerTakePicture.symptom.js
@@ -0,0 +1,98 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, Image, Dimensions } from 'react-native';
+import { Button } from 'react-native-elements'
+import ImagePicker from 'react-native-image-picker'
+import ImageResizer from 'react-native-image-resizer'
+
+const imageOptions = {
+    title: 'Choose a photo',
+    storageOptions: {
+        skipBackup: true,
+        path: 'images'
+    }
+}
+
+var { width } = Dimensions.get('window')
+
+export default class AnswerTakePicture extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            loading: false,
+            path: '',
+            filename: '',
+            timestamp: '',
+        }
+    }
+
+    //KOPAI: 
+    _onPress = async () => {
+        ImagePicker.showImagePicker(imageOptions, response => {
+            if (response.didCancel) {
+                console.log('User cancelled image picker')
+            } else if (response.error) {
+                console.log('ImagePicker Error: ', response.error)
+            } else if (response.customButton) {
+                console.log('User tapped custom button: ', response.customButton)
+            } else {
+                let imageSource = { uri: 'data:image/jpeg;base64,' + response.data }
+                let imageUri = response.uri + ''
+                let filename = response.fileName + ''
+                let timestamp = response.timestamp + ''
+                ImageResizer.createResizedImage(imageUri, 328, 495, 'JPEG', 60).then(async response => {
+                    let path = response.path + ''
+                    await this.setState({ path, filename, timestamp })
+                    this.setState({ imageSource: imageUri })
+                })
+                    .then(() => {
+                        console.log(this.state)
+                        this.props._setCurrentPatientAnswer(this.props.answer)
+                        this.props._setImage(this.state.path, this.state.filename, this.state.timestamp)
+                    })
+                    .catch(err => {
+                        console.log('error in choose photo ', err)
+                        Alert.alert('ผิดพลาด! ไม่สามารถเลือกรูปภาพ')
+                    })
+            }
+        })
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                {/*<Image source={require('../assets/images/borg_great.png')} resizeMode = 'cover' style={{flex: 1, alignSelf: 'center', overflow: 'visible', margin: 10}} />*/}
+                <Image source={{ uri: this.state.imageSource }} style={styles.image} resizeMode='cover'></Image>
+                <Button
+                    title="เลือกรูปภาพ"
+                    onPress={this._onPress}
+                    buttonStyle={styles.button}
+                    fontFamily="Kanit-Regular"
+                    rounded
+                    raised
+                    backgroundColor="#80cdc0"
+                />
+
+            </View>
+        )
+    }
+}
+
+const styles = {
+    container: {
+        flex: 1
+    },
+    button: {
+        margin: 3,
+        flex: 1
+    },
+    image: {
+        // height: 300,
+        // width: width * 0.5,
+        margin: 20,
+        alignSelf: 'center',
+        // overflow: 'visible',
+        // flex: 1,
+        height: 300,
+        width: '100%'
+    }
+}
