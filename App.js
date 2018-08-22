8175b4726877dc3d23ca8d6fefeb5b21415aebd0
zigext <zigext@gmail.com>
2018-08-23 03:37:03 +0700

    Change symptom taking screen

--- App.js
+++ App.js
@@ -14,7 +14,8 @@ import {
 import { BottomNavigation, COLOR, ThemeProvider, Toolbar, Card, Checkbox, RadioButton, Icon, IconToggle } from 'react-native-material-ui';
 import VectorIcon from 'react-native-vector-icons/MaterialIcons'
 import SymptomTakingScreen from './screens/SymptomTakingScreen'
-
+import SymptomTakingScreen2 from './screens/SymptomTakingScreen2'
+import SymptomTakingScreen3 from './screens/SymptomTakingScreen3'
 
 type Props = {};
 export default class App extends Component<Props> {
@@ -22,7 +23,7 @@ export default class App extends Component<Props> {
     return (
       <ThemeProvider uiTheme={uiTheme}>
 
-        <SymptomTakingScreen />
+        <SymptomTakingScreen3 />
 
       </ThemeProvider>
     );
