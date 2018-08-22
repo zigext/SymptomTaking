ffeda88a9691de538005df0cf893b2db45a8f0f0
zigext <zigext@gmail.com>
2018-08-23 03:34:48 +0700

    User data

---
+++ UcareData/mockdata.js
@@ -0,0 +1,47 @@
+const User={
+    "createdAt": "2018-05-28T03:55:50.504Z",
+    "passwordChangePending": false,
+    "nationalID": "1239759304097",
+    "modifiedAt": "2018-06-12T13:36:01.953Z",
+    "info": {
+      "currentDrug": "ยาแก้ปวด",
+      "weight": "45",
+      "height": "165",
+      "ocupation": "นักศึกษา",
+      "allergicToFood": "กุ้ง",
+      "isDrinking": false,
+      "isSmoking": false,
+      "allergicToDrug": "",
+      "sex":"F"
+    },
+    "FCMTokens": {
+      "dkF1DszhXfI:APA91bGILeAWla-WFIehH4uWcyE3xTAuHVMfnch08ULwoA9dnPjTjilfHx_O8iMEtOnKnsiw_379_GomFpUxaRrpmvgENWKHcnUjNLiu07ezSGW_YSXee24NdHMefzVKJmIK-4wzYwkP": true,
+      "dUhDP3G-YeI:APA91bHXs3n8JJTMsSpVNX75-6W96P2-iEU8fIdqAStw9prqjJYp8hCjzjpulHFY38O3uCS5W6s2Y0wl75amXKnhteLt0MJJTTcbenxNANwp3QO_nVlTXwckyb53fEhKou_hX-QGTL_W": true,
+      "cwR0rsHb5_k:APA91bEci-wxd6R7LvFcqkq7cUpe9X_AZ8Ty29zdxkNEBKdFYIQikGR7QTh7jB4FT9XPCEXkAzexBnj3Joocw1diJqRYZePQM2oUav549hg6dtZ9TGtwC3rp1W35pDy4n9InkIDr6Zvx": true,
+      "cIpgOxCHciA:APA91bFH0vv5hiEgRC-C8_3kCTs4449cPTtPal9AGynBaBsy5z-gPBc3nbhWsEe5cUHtQKW9AFVXfUuK2Q20EAisi1eNhHKmK6rlxxhE5OQ2fXPDX9SL9BzIUGzicAfvdx_91m6c1eMS": true,
+      "ccaTPR4stG8:APA91bHzB7e3u2xuyci75jPOja_T06E7dUvXeEwEkT03cqg10zToox2OEJiBTKrjXjNDR2f48cLdSJJEUPbSAbNI9Lz-PXdVNSLXimp2HjsGFe2iEnXLs6_WiC_hK6PkjzS1AK52V-lr": true,
+      "eGvn-vEUDeg:APA91bE8byMt85HNz9HbcR0GxmGq-X2lbpeqn8CS9EEg0DntyPiI-LOjCiH0WhRRt0iRYXwsACd5cAy5s_GYs_Abf7_kxodkC9FqQoS0_HJTUFpYHgSPIiYrAnKS23nOc5dL_hNP5gjB": true,
+      "eBYLwjbt-3E:APA91bFV8S7zKVz5WanZnQL4hYtEVe9EYw9gDHr086mL8tpAEyGxSLJw4aOZ9CNdbwGjbv8G0UxPxjJ0bmSs_TJQK1W8_Uz8aPWfZECkCQuHhb2XhN81nNljEriAeG7qRguwxklpaODv": true,
+      "c-nPodkUuCs:APA91bF7TfLxFKAe9Q-yNxTKxAOFlRIoI_nS0_zvWSOvNsyJARrnLStOyZzIx1VMrk4MSviZGZXB0zcBpdBbsJGEA0Fkxwpn3zdm8ZFtr1GCB80v7VPw3TkBlOwz7ucQF1mnNzJMKNQE": true,
+      "e1ioJgnUn5I:APA91bFWqrE6qeBEqgJ-twrH99omoU0NZRVuK0u6LqNhz_2UDkOuzfbySNB5EjHigxVP5H8EGdj1IygtNMXtX4VDBUOrWshmpIVmMHc0BnLBqrL09_Vp65r9hO_-gKS2zXhw6jAVDx3V": true,
+      "deeOOxAU0Ys:APA91bGSZjdVkouJ8FYW4TKQ637h_xRQrrHXuOKnc87Bx23vbRAmaPdUFcEF-NzjBVIIUz_0DbYnv8RCBcyGSSHmljRUB4zGp1r3-wuknlgnHE5i31xVEKYJZC6-g1OHaCIRv6Ug81Vm": true,
+      "f7x2Y-HqDbs:APA91bHdINMm5ZAyVWHXsAuhIz4PblGs4qQfoE-6_5i87h4BTElJbkZCd3PirWvkv7X_IST9o9zk18wUH2cTHxdcz75W3IWKhNHsNLwP5SfcRg-51krS4Y_3ceUhuAdkkhYTncoH3oN_": true,
+      "dOKdicO9jtw:APA91bEPuHPfO4uCJOI2duHheJOeNa686aWZJSfoFU5M3j8L6UFE6WhUqrXrLYLqRQoO5FHW3iQiTaB-iAK2Me6nY2a_OScIx-FIfGZ-IE4dfvhRxdJUJvsD2AWcXKOE9oVzBZ_6ylm2": true,
+      "dBQCVdAAHF8:APA91bFLO1fzpclZxDJWlu18ymzrfsofmqK7_gcyOxz6AqgSBSXy2f_J1WHIEm2kivg5MGme4LJ1FHJ9ZzJzHlqFqATXMMYH8EcF9g4FZ7HPX1dujU8xQJ2IPAjjJ2sfFwcHT0OGBe4I": true,
+      "dlLeMYdzpBg:APA91bEPU8nQ7cxrzyiiopt3mdOJ3AtA2uE6CTtMi_5ePInpkfzOALHAr51mM2ijrctt3EDaHIAPD8ku-NoqXlcdLaqgxl8DSHzuSCraZSyC6DLZHO56pOAbMrmmTZqzVIpOSAW7xeqS": true,
+      "ddhnQm8zv1k:APA91bHuK6sT09dgq1pz7OwiW0Yv_f28k8r0M1vloL0qRLJSp286b8qQFXxeU43WdmR8EGM-vlul-dfnrmLeTSH17Lyy1Q15KEch0tw1UOG_WI3ZtCP0IfJEjNNcI_PFRvFYAHirtlDO": true
+    },
+    "photoURL": "https://firebasestorage.googleapis.com/v0/b/ucare-chat-staging.appspot.com/o/profilePicture%2FVykyotWg0uVyIBp5u1eF8V5XAOs2%2Fb4b90cb6-563a-42f3-b2b1-4667d8d72846.jpeg?alt=media&token=651d4f87-c6a7-4185-b7fc-38c7c4915ba2",
+    "birth": "1984-05-28",
+    "name": {
+      "title": "นางสาว",
+      "firstName": "สุ",
+      "lastName": "นานา"
+    },
+    "userType": "patient",
+    "phoneNumber": "0873985100",
+    "healthCenter": "Mk3mtdpCTqu7pHPn5Q0V"
+  }
+
+  export default User
+
